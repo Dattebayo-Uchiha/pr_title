30708456dcdfd1e3aa58142162e25cba56abae2d
Vidyush <vidushbakshi96@gmail.com>
2021-09-17 13:59:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -44,10 +44,9 @@ async function run() {
         
         // Check if description pass regex
         rb ="[[a-bA-B].+-[0-9]+]"
-        rb2="REL-.+[0-9]|CGT-.+[0-9]|PLATFORM-.+[0-9]"
-        console.log(rb2.match(desc))
-        console.log(/^(\[REL-.+[0-9]\]|\[CGT-\]|\[PLATFORM-\])$/.test(desc))
-        if (/^(\[REL-\]|\[CGT-\]|\[PLATFORM-\])$/.test(desc)){
+        rb2=/REL-.+[0-9]|CGT-.+[0-9]|PLATFORM-.+[0-9]/
+        console.log(rb2.test(desc))
+        if (rb2.test(desc)){
             console.log(/^(\[REL-\]|\[CGT-\]|\[PLATFORM-\])$/.test(desc))
             }
         // Check if title pass regex
